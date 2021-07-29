adcbd51da4579cbf0e3ca14c3cc8358457ead348
MuhammadTaimoor795 <muhammadtaimoor049@gmail.com>
2021-07-29 16:32:56 +0500

    core issue solve

--- server.js
+++ server.js
@@ -1,103 +1,64 @@
-// Dotenv handy for local config & debugging
-require('dotenv').config()
-
-// Core Express & logging stuff
-const express = require('express')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
-const app = express()
-const models = require("./models/index");
-const { sequelize } = require("./models");
-
-const errorHandler = require('./src/middleware/errorHandler')
+const express = require("express");
+const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const config = require("./config");
+const errorHandler = require("./src/middleware/errorHandler");
+const logger = require("morgan");
+const passport = require("passport");
+//const passportConfig = require("./passport-config");
+const sessions = require("express-session");
+const db = require("./models");
+const bcrypt = require("bcrypt");
+const cookie = require("cookie-parser");
 
-// Logging
-app.use(logger('dev'))
 
-
-//cors
-const cors = require('cors')
-const helmet = require('helmet')
+const { sequelize } = require("./models");
+const cors = require("cors");
 app.use(
-cors({
-origin: (origin, callback) => callback(null, true),
-credentials: true,
-})
+  cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
+  })
 );
+app.use(cookie());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-     next();
-});
 
-//wearing a helmet 
+app.use(
+  sessions({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(helmet());
 
-// Parsing middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, "../data")));
-// Routes & controllers
 app.get("/", (req, res) => res.json({ msg: "Welcome to Lawyers api" }));
 
 app.use("/lawyer", require("./src/routes/lawyer-route"));
 app.use("/client", require("./src/routes/client-route"));
 app.use("/common", require("./src/routes/common-route"));
 
-
-
-// Catch all route, generate an error & forward to error handler
-app.use(function (req, res, next) {
-  let err = new Error('Not Found')
-  err.status = 404
-  
-
-  next(err)
-})
-
+//app.use(express.static(path.join(__dirname, "../public")));
 app.use(errorHandler)
 
-// // Error handler
-// app.use(function (err, req, res, next) {
-//   console.error(`### ERROR: ${err.message}`)
-  
-//   res.status(err.status || 500)
-//   res.json({
-//     title: 'Error',
-//     message: err.message,
-//     error: err,
-//   })
-// })
-
-// Get values from env vars or defaults where not provided
-let port = process.env.PORT || 7000
-
-// const admin = async()=>{
-//   try {
-//     const password = 'admin@123'
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-//     await models.admin.create({
-//       email:'awais.satti01@gmail.com',
-//       password:hashedPassword
-//   })
-//   } catch (error) {
-//     console.log(error.message)
-//   }
-// }
-// admin();
+app.use((req, res, next) => {
+  const err = new Error("Not found!");
+  err.status = 404;
+  return res.status(404).json({
+    error: "API not found ",
+  });
+  next(err);
+});
 
 
-// Start the server
-app.listen(port || 7000, async () => {
-  console.log(`Server Started on port ${port}`);
+app.listen("7000", async () => {
+  console.log("Server Start");
+  console.log(`http://localhost:7000`);
   await sequelize.authenticate();
   //await sequelize.sync({force:true , alter: true});
   console.log("DB connected");
 });
-
-module.exports = app
